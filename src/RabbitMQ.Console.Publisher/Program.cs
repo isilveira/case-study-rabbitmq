04965f1eb8cd1541a70dc07fbb2ff2a5a50e70c4
Ítalo Silveira <italobrian@gmail.com>
2019-01-31 18:57:21 -0300

    Send message one started

--- src/RabbitMQ.Console.Publisher/Program.cs
+++ src/RabbitMQ.Console.Publisher/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace RabbitMQ.Console.Publisher
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Console.Publisher
 {
     class Program
     {
@@ -20,19 +23,56 @@
                         {
                             System.Console.WriteLine("Option one!");
 
+                            SendMessageOne();
+
                             System.Console.ReadLine();
                         }
                         break;
                     default:
                         {
-                            
-                            
+
+
                         }
                         break;
                 }
             } while (op != "x" && op != "X");
             System.Console.WriteLine("  exiting...");
             System.Console.ReadLine();
+        }
+
+        private static void SendMessageOne()
+        {
+            var factory = new ConnectionFactory()
+            {
+                //VirtualHost = "",
+                HostName = "hornet",
+                Port = 1883,
+                UserName = "tvudjnyz:tvudjnyz",
+                Password = "Qvhh8Gv8qs6BHOGq1EQgO0SPah4d-Ywd",
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    string message = "Hello World! MessageOne";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    System.Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
         }
     }
 }
